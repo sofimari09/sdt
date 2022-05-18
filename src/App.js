b46da0840cd52f2b46c5e0d0ba28ad90ee876ebf
unknown <Desofiyamariya@gmail.com>
2022-05-18 18:24:07 +0300

    change sth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Home from './Home';
 import About from './About';
+import New from './New';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
      <h1>React hgnbkgkj</h1>
      <Router>
        <Routes>
-       
+       <Route path = "/new" element = {<About/>}/>
        <Route path = "/about" element = {<About/>}/>
        <Route path = "/" element = {<Home/>}/>
        </Routes>
